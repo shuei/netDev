7349f9be80be927f61405cd63d778ff93edce8e1
Shuei Yamada <shuei@post.kek.jp>
2021-09-16 11:43:58 +0900

    Code cleaned up.

--- src/devChinoLogL.c
+++ src/devChinoLogL.c
@@ -20,17 +20,12 @@
 /* Modification Log:
  * -----------------
  */
-#include        <dbFldTypes.h>
 
-#include        "drvNetMpf.h"
-#include        "devNetDev.h"
-
-#ifndef EPICS_REVISION
-#include <epicsVersion.h>
-#endif
-#if EPICS_REVISION == 14 && EPICS_MODIFICATION >= 2
+#include <dbFldTypes.h>
 #include <epicsExport.h>
-#endif
+
+#include "drvNetMpf.h"
+#include "devNetDev.h"
 
 #define CHINOL_CMND_LENGTH 8  /* not correct */
 #define CHINOL_UDP_PORT    11111
@@ -40,367 +35,341 @@
 
 //LOCAL int finsUseSamePortNumber = MPF_SAMEPORT;
 LOCAL unsigned short calCRC(unsigned char cd[], int nmax);
-LOCAL int TwoRawToVal(unsigned char bu[],int offset, float *res );
-LOCAL long chino_parse_link(
-			  struct link *,
-			  struct sockaddr_in *,
-			  int *,
-			  void *
-			  );
+LOCAL int TwoRawToVal(unsigned char bu[],int offset, float *res);
+LOCAL long chino_parse_link(struct link *, struct sockaddr_in *, int *, void *);
 LOCAL int chino_get_protocol(void);
 LOCAL void *chino_calloc(int, int, int, int, int);
 
-
-typedef struct
-{
-  int      unit;
-  int      type;
-  int      chan;
-  int      bit;
-  int      width;
-  int      slvA;
-  int      cmd;
-  int      start;
-  int      ndata;
-  int      data_trans;
-  char    *lopt;
+typedef struct {
+    int      unit;
+    int      type;
+    int      chan;
+    int      bit;
+    int      width;
+    int      slvA;
+    int      cmd;
+    int      start;
+    int      ndata;
+    int      data_trans;
+    char    *lopt;
 } CHINOL_LOG;
 
 LOCAL long chino_config_command(uint8_t *, int *, void *, int, int, int *, CHINOL_LOG *, int);
 LOCAL long chino_parse_response(uint8_t *, int *, void *, int, int, int *, CHINOL_LOG *, int);
 
-int chinoLogLUseTcp = 1;
-
+int chinoLogLUseTcp = 1; // we'd better make this static until we are able to change this via iocsh command line.
 
 LOCAL int chino_get_protocol(void)
 {
-  if (chinoLogLUseTcp)
-    {
-      return MPF_TCP;
+    if (chinoLogLUseTcp) {
+        return MPF_TCP;
     }
 
-  return MPF_UDP;
+    return MPF_UDP;
 }
-
 
 LOCAL void *chino_calloc(
-		       int unit,
-		       int type,
-		       int chan,
-		       int bit,
-		       int width
-		       )
+                         int unit,
+                         int type,
+                         int chan,
+                         int bit,
+                         int width
+                         )
 {
-  CHINOL_LOG *d;
-
-  d = (CHINOL_LOG *) calloc(1, sizeof(CHINOL_LOG));
-  if (!d)
-    {
-      errlogPrintf("devChinoLogL: calloc failed\n");
-      return NULL;
+    CHINOL_LOG *d = (CHINOL_LOG *) calloc(1, sizeof(CHINOL_LOG));
+    if (!d) {
+        errlogPrintf("devChinoLogL: calloc failed\n");
+        return NULL;
     }
-      
-  d->unit   = unit;
-  d->type   = type;
-  d->chan   = chan;
-  d->bit    = bit;
-  d->width  = width;
 
-  return d;
+    d->unit   = unit;
+    d->type   = type;
+    d->chan   = chan;
+    d->bit    = bit;
+    d->width  = width;
+
+    return d;
 }
 
+#include "devWaveformChinoLogL.c"
 
-
-#include        "devWaveformChinoLogL.c"
-
-
-
-
-
 /*********************************************************************************
  * Link field parser for command/response I/O
  *********************************************************************************/
 LOCAL long chino_parse_link(
-			  struct link *plink,
-			  struct sockaddr_in *peer_addr,
-			  int *option,
-			  void *device
-			  )
+                            struct link *plink,
+                            struct sockaddr_in *peer_addr,
+                            int *option,
+                            void *device
+                            )
 {
-  char *protocol = NULL;
-  char *unit  = NULL;
-  char *type  = NULL;
-  char *addr  = NULL;
-  char *route = NULL;
-  char *lopt  = NULL;
-  CHINOL_LOG *d = (CHINOL_LOG *) device;
+    char *protocol = NULL;
+    char *unit  = NULL;
+    char *type  = NULL;
+    char *addr  = NULL;
+    char *route = NULL;
+    char *lopt  = NULL;
+    CHINOL_LOG *d = (CHINOL_LOG *) device;
 
-  if (parseLinkPlcCommon(
-			 plink,
-			 peer_addr,
-			 &protocol,
-			 &route, /* dummy */
-			 &unit,
-			 &type,
-			 &addr,
-			 &lopt
-			 ))
-    {
-      errlogPrintf("devChinoLogL: illeagal input specification\n");
-      return ERROR;
+    if (parseLinkPlcCommon(
+                           plink,
+                           peer_addr,
+                           &protocol,
+                           &route, /* dummy */
+                           &unit,
+                           &type,
+                           &addr,
+                           &lopt
+                           )) {
+        errlogPrintf("devChinoLogL: illeagal input specification\n");
+        return ERROR;
     }
 
-  errlogPrintf("chino parse link\n");
-  if (sscanf(unit,"%x",&d->slvA) !=1)
-    {
-      errlogPrintf("devChinoLogL :cannot get slave address\n");
-    }
-  if (sscanf(type,"%x",&d->cmd) !=1)
-    {
-      errlogPrintf("devChinoLogL :cannot get command\n");
+    LOGMSG("chino parse link\n");
+
+    if (unit) {
+        if (sscanf(unit,"%x",&d->slvA) != 1) {
+            errlogPrintf("devChinoLogL :cannot get slave address\n");
+            return ERROR;
+        }
+        LOGMSG("devChinoLogL: slave address: %d\n", d->slvA);
+    } else {
+        errlogPrintf("devChinoLogL: no slave address specified\n");
+        return ERROR;
     }
 
-
-   if (sscanf(addr,"%d",&d->start) != 1)
-    {
-      errlogPrintf("devChinoLogL : cannot get start address\n");
+    if (type) {
+        if (sscanf(type,"%x",&d->cmd) != 1) {
+            errlogPrintf("devChinoLogL :cannot get command\n");
+            return ERROR;
+        }
+        LOGMSG("devChinoLogL: command\n", d->cmd);
+    } else {
+        errlogPrintf("devChinoLogL: no command specified\n");
+        return ERROR;
     }
 
-   if (sscanf(lopt,"%d",&d->data_trans) !=1)
-    {
-      errlogPrintf("devChinoLogL : cannot get transfer length\n");
+    if (addr) {
+        if (sscanf(addr,"%d",&d->start) != 1) {
+            errlogPrintf("devChinoLogL : cannot get start address\n");
+            return ERROR;
+        }
+    } else {
+        errlogPrintf("devChinoLogL: no start address specified\n");
+        return ERROR;
     }
-   /* errlogPrintf("parse start %d trans %d\n",d->start, d->data_trans); */
-  return OK;
+
+    if (lopt) {
+        if (sscanf(lopt,"%d",&d->data_trans) != 1) {
+            errlogPrintf("devChinoLogL : cannot get transfer length\n");
+            return ERROR;
+        }
+    } else {
+        //errlogPrintf("devChinoLogL: no transfer length specified\n");
+        //return ERROR;
+        d->data_trans = 12;
+        LOGMSG("devChinoLogL: no transfer length specified. Assuming %d\n", d->data_trans);
+    }
+
+    LOGMSG("parse start %d trans %d\n",d->start, d->data_trans);
+    return OK;
 }
 
-
-
-
-
 /******************************************************************************
  * Command constructor for command/response I/O
  ******************************************************************************/
 LOCAL unsigned short calCRC(unsigned char cd[], int nmax)
 {
-  int i,j;
-  unsigned short iCRC;
-  unsigned short iCY,iP;
-  unsigned char iC1,iC2;
-                                                                                
-  iCRC = 0xffff;
-                                                                                
-  for (i=0;i<nmax;i++)
-    {
-      iCRC = iCRC ^ cd[i];
-      for (j=1; j<=8; j++)
-        {
-          iCY = iCRC & 0x1;
-          if ((iCRC & 0x8000) == 0x8000)
-            {
-              iP = 0x4000;
-              iCRC = iCRC & 0x7fff;
+    unsigned short iP  = 0x0000;
+
+    unsigned short iCRC = 0xffff;
+
+    for (int i=0; i<nmax; i++) {
+        iCRC = iCRC ^ cd[i];
+        for (int j=1; j<=8; j++) {
+            unsigned short iCY = iCRC & 0x1;\
+
+            if ((iCRC & 0x8000) == 0x8000) {
+                iP = 0x4000;
+                iCRC = iCRC & 0x7fff;
+            } else {
+                iP = 0x0;
             }
-          else
-            {
-              iP = 0x0;
+
+            iCRC = iCRC>>1;
+            iCRC = iCRC | iP;
+            if (iCY == 1) {
+                iCRC = iCRC ^ 0xa001;
             }
-          iCRC = iCRC>>1;
-          iCRC = iCRC | iP;
-          if (iCY == 1){
-            iCRC = iCRC ^ 0xa001;
-          }
         }
     }
-  if ((iCRC & 0x8000) == 0x8000)
-    {
-      iP = 0x80;
-      iCRC = iCRC & 0x7fff;
+
+    if ((iCRC & 0x8000) == 0x8000) {
+        iP = 0x80;
+        iCRC = iCRC & 0x7fff;
+    } else {
+        iP = 0;
     }
-  else
-    {
-      iP = 0;
-    }
-  iC1 = iCRC & 0xff;
-  iC2 = ((iCRC & 0x7f00) >>8) | iP;
-                                                                                
-  return iC1*256 + iC2;
-                                                                                
+
+    unsigned char iC1 = iCRC & 0xff;
+    unsigned char iC2 = ((iCRC & 0x7f00) >>8) | iP;
+
+    return iC1*256 + iC2;
 }
 
-LOCAL long chino_config_command(
-			      uint8_t *buf,    /* driver buf addr     */
-			      int     *len,    /* driver buf size     */
-			      void    *bptr,   /* record buf addr     */
-			      int      ftvl,   /* record field type   */
-			      int      ndata,  /* n to be transferred */
-			      int     *option, /* direction etc.      */
-			      CHINOL_LOG *d,
-			      int      sid
-			      )
+LOCAL long chino_config_command(uint8_t *buf,    /* driver buf addr     */
+                                int     *len,    /* driver buf size     */
+                                void    *bptr,   /* record buf addr     */
+                                int      ftvl,   /* record field type   */
+                                int      ndata,  /* n to be transferred */
+                                int     *option, /* direction etc.      */
+                                CHINOL_LOG *d,
+                                int      sid
+                                )
 {
-  int nwrite;
-  int n;
+    LOGMSG("devChinoLogL: chino_config_command(0x%08x,%d,0x%08x,%d,%d,%d,0x%08x)\n",
+           buf,*len,bptr,ftvl,ndata,*option,d,0,0);
 
-  int resCRC;
+    int n = ndata;
+    int nwrite = isWrite(*option) ? (d->width)*n : 0;
 
-  LOGMSG("devChinoLogL: chino_config_command(0x%08x,%d,0x%08x,%d,%d,%d,0x%08x)\n",
-	 buf,*len,bptr,ftvl,ndata,*option,d,0,0);
-
-  n = ndata;
-
-  nwrite = isWrite(*option) ? (d->width)*n : 0;
-
-  if (*len < CHINOL_CMND_LENGTH + nwrite)
-    {
-      errlogPrintf("devChinoLogL: buffer is running short\n");
-      return ERROR;
+    if (*len < CHINOL_CMND_LENGTH + nwrite) {
+        errlogPrintf("devChinoLogL: buffer is running short\n");
+        return ERROR;
     }
 
-  buf[ 0] = d->slvA;                    /* slave address */
-  buf[ 1] = d->cmd;                       /* command     */
-  buf[ 2] = d->start>>8;                /* start address (H)*/
-  buf[ 3] = d->start;                   /* start address (L)*/
-  buf[ 4] = 0x00;                      /* number (H) */
-  buf[ 5] = (d->data_trans)*2;              /* number (L) 0x0c */
- 
-  resCRC = calCRC(buf,6);
+    buf[ 0] = d->slvA;           /* slave address */
+    buf[ 1] = d->cmd;            /* command */
+    buf[ 2] = d->start>>8;       /* start address (H) */
+    buf[ 3] = d->start;          /* start address (L) */
+    buf[ 4] = 0x00;              /* number (H) */
+    buf[ 5] = (d->data_trans)*2; /* number (L) 0x0c */
 
-  buf[ 6] = resCRC>>8;                     /* crc (H) */
-  buf[ 7] = resCRC     ;                  /* crc (L) */
+    int resCRC = calCRC(buf,6);
 
-  /*  errlogPrintf("devChinoLogL: command = %x %x %x %x %x %x %x %x\n",buf[0],buf[1],buf[2],buf[3],buf[4],buf[5],buf[6],buf[7]); */
-  *len = CHINOL_CMND_LENGTH;
+    buf[ 6] = resCRC>>8;          /* crc (H) */
+    buf[ 7] = resCRC;             /* crc (L) */
 
-  /*  if (isWrite(*option))
-    {
-      if (fromRecordVal(
-			&buf[CHINOL_CMND_LENGTH],
-			d->width,
-			bptr,
-			d->noff,
-			ftvl,
-			n,
-			CHINOL_NEEDS_SWAP
-			))
-	{
-	  errlogPrintf("devChinoLogL: illeagal command\n");
-	  return ERROR;
-	}
+    LOGMSG("devChinoLogL: command = %x %x %x %x %x %x %x %x\n",buf[0],buf[1],buf[2],buf[3],buf[4],buf[5],buf[6],buf[7]);
+
+    *len = CHINOL_CMND_LENGTH;
+
+#if 0
+    if (isWrite(*option)) {
+        if (fromRecordVal(
+                          &buf[CHINOL_CMND_LENGTH],
+                          d->width,
+                          bptr,
+                          d->noff,
+                          ftvl,
+                          n,
+                          CHINOL_NEEDS_SWAP
+                          )) {
+            errlogPrintf("devChinoLogL: illeagal command\n");
+            return ERROR;
+        }
     }
-  */
-  /*
-  nread  = isRead(*option)? (d->width)*n:0;
-  return (CHINOL_DATA_OFFSET + nread);
-  */
-  return 0;
+#endif
+
+#if 0
+    nread  = isRead(*option)? (d->width)*n:0;
+    return (CHINOL_DATA_OFFSET + nread);
+#endif
+    return 0;
 }
 
-
-
-
 /*******************************************************************************
  * Response parser for command/response I/O
  *******************************************************************************/
-LOCAL int TwoRawToVal(unsigned char bu[],int offset, float *res )
+LOCAL int TwoRawToVal(unsigned char bu[], int offset, float *res)
 {
+    float f1;
 
-  float f1;
-  
-  if ((bu[offset+2] & 0x20) == 0x20){
-    f1 = bu[offset]*256 + bu[offset+1];
-      }
-  else if ((bu[offset] & 0x80) == 0x80){
-    f1 = (bu[offset]-255)*256 +(bu[offset+1]-255);
-  }
-  else{
-    f1 = bu[offset]*256+bu[offset+1];
-  }
+    if ((bu[offset+2] & 0x20) == 0x20) {
+        f1 = bu[offset]*256 + bu[offset+1];
+    }
+    else if ((bu[offset] & 0x80) == 0x80) {
+        f1 = (bu[offset]-255)*256 + (bu[offset+1]-255);
+    }
+    else {
+        f1 = bu[offset]*256 + bu[offset+1];
+    }
 
-  switch (bu[offset+3] & 0x0f)
-    {
+    switch (bu[offset+3] & 0x0f) {
     case 0:
-      *res = f1;
-      break;
+        *res = f1;
+        break;
     case 1:
-      *res = f1*0.1;
-      break;
+        *res = f1*0.1;
+        break;
     case 2:
-      *res = f1*0.01;
-      break;
+        *res = f1*0.01;
+        break;
     case 3:
-      *res = f1*0.001;
-      break;
+        *res = f1*0.001;
+        break;
     case 4:
-      *res = f1*0.0001;
-      break;
+        *res = f1*0.0001;
+        break;
     default:
-      return -1;
+        return -1; // this value is passed to chino_parse_response() but will be ignored
     }
 
-  return (bu[offset+3] & 0xf0);
-} 
-
-
-LOCAL long chino_parse_response(
-			      uint8_t *buf,    /* driver buf addr     */
-			      int     *len,    /* driver buf size     */
-			      void    *bptr,   /* record buf addr     */
-			      int      ftvl,   /* record field type   */
-			      int      ndata,  /* n to be transferred */
-			      int     *option, /* direction etc.      */
-			      CHINOL_LOG *d,
-			      int      sid
-			      )
-{
-  int i;
-  int ret;
-  float temp[1000],*rawVal,*ptemp;
-
-  rawVal = bptr;
-
-  LOGMSG("devChinoLogL: chino_parse_response(0x%08x,%d,0x%08x,%d,%d,%d,0x%08x)\n",
-	 buf,len,bptr,ftvl,ndata,*option,d,0,0);
- 
-  /*  for (i=0;i<12;i++)
-    {
-       errlogPrintf("devChinoLogL buffer %d = %x %x %x %x\n",i*4,buf[CHINOL_DATA_OFFSET+i*4],buf[CHINOL_DATA_OFFSET+i*4+1],buf[CHINOL_DATA_OFFSET+i*4+2],buf[CHINOL_DATA_OFFSET+i*4+3]);
-       } */
-  if (isRead(*option))
-    {
-      for (i=0;i<(d->data_trans);i++)
-	{
-	  ret = TwoRawToVal(buf,CHINOL_DATA_OFFSET+i*4,&temp[i]);
-	  if (ret != 0){
-	    switch (ret & 0xf0){
-	    case 0x10:
-	      temp[i]=-10.0;
-	      break;
-	    case 0x20:
-	      temp[i]=10.0;
-	      break;
-	    case 0x30:
-	      temp[i]= -1000.0;
-	      break;
-	    default:
-	      temp[i] = -1000.0;
-	      /* errlogPrintf("devGhinoLog Emergency code for %d =%x\n",i,ret); */
-	    }
-	  }
-	}
-    }
-  
-  ndata = d->data_trans;
-  ptemp = temp;
-  i= ndata;
-  while (i--)
-    {
-      *rawVal++ = (float) *ptemp ++;
-    }
-  ret = 0;
-
-  return ret;
+    return (bu[offset+3] & 0xf0);
 }
 
+LOCAL long chino_parse_response(uint8_t *buf,    /* driver buf addr     */
+                                int     *len,    /* driver buf size     */
+                                void    *bptr,   /* record buf addr     */
+                                int      ftvl,   /* record field type   */
+                                int      ndata,  /* n to be transferred */
+                                int     *option, /* direction etc.      */
+                                CHINOL_LOG *d,
+                                int      sid
+                                )
+{
+    LOGMSG("devChinoLogL: chino_parse_response(0x%08x,%d,0x%08x,%d,%d,%d,0x%08x)\n",
+           buf,len,bptr,ftvl,ndata,*option,d,0,0);
 
+#if 0
+    for (int i=0;i<12;i++) {
+        errlogPrintf("devChinoLogL buffer %d = %x %x %x %x\n",i*4,buf[CHINOL_DATA_OFFSET+i*4],buf[CHINOL_DATA_OFFSET+i*4+1],buf[CHINOL_DATA_OFFSET+i*4+2],buf[CHINOL_DATA_OFFSET+i*4+3]);
+    }
+#endif
 
+    float temp[1000];
+    int ret = 0;
+    if (isRead(*option)) {
+        for (int i=0; i<(d->data_trans); i++) {
+            ret = TwoRawToVal(buf, CHINOL_DATA_OFFSET+i*4, &temp[i]);
+            if (ret != 0) {
+                switch (ret & 0xf0) {
+                case 0x10:
+                    temp[i] = -10.0;
+                    break;
+                case 0x20:
+                    temp[i] = 10.0;
+                    break;
+                case 0x30:
+                    temp[i] = -1000.0;
+                    break;
+                default:
+                    temp[i] = -1000.0; // we'd better to change this value to someting else, e.g. -9999.
+                    errlogPrintf("devGhinoLog Emergency code for %d =%x\n",i,ret);
+                }
+            }
+        }
+    }
+
+    ndata = d->data_trans;
+    float *ptemp = temp;
+    float *rawVal = bptr;
+    int i = ndata;
+
+    while (i--) {
+        *rawVal++ = (float) *ptemp ++;
+    }
+
+    ret = 0; // returns always zero
+    return ret;
+}
--- src/devWaveformChinoLogL.c
+++ src/devWaveformChinoLogL.c
@@ -3,7 +3,8 @@
 /* Modification Log:
  * -----------------
  */
-#include        <waveformRecord.h>
+
+#include <waveformRecord.h>
 
 /***************************************************************
  * Waveform (command/response IO)
@@ -14,111 +15,95 @@ LOCAL long config_waveform_command(struct dbCommon *, int *, uint8_t *, int *, v
 LOCAL long parse_waveform_response(struct dbCommon *, int *, uint8_t *, int *, void *, int);
 
 INTEGERDSET devWfChinoLogL = {
-  5,
-  NULL,
-  netDevInit,
-  init_waveform_record,
-  NULL,
-  read_waveform
+    5,
+    NULL,
+    netDevInit,
+    init_waveform_record,
+    NULL,
+    read_waveform
 };
 
-#if EPICS_REVISION == 14 && EPICS_MODIFICATION >= 2
 epicsExportAddress(dset, devWfChinoLogL);
-#endif
-
 
 LOCAL long init_waveform_record(struct waveformRecord *pwf)
 {
-  CHINOL_LOG *d;
+    CHINOL_LOG *d  = chino_calloc(0, 0, 0, 0, 2);
 
-  d = chino_calloc(0, 0, 0, 0, 2);
-
-
-  return netDevInitXxRecord(
-			    (struct dbCommon *) pwf,
-			    &pwf->inp,
-			    MPF_READ | CHINOL_GET_PROTO | DEFAULT_TIMEOUT,
-			    d,
-			    chino_parse_link,
-			    config_waveform_command,
-			    parse_waveform_response
-			    );
+    return netDevInitXxRecord((struct dbCommon *) pwf,
+                              &pwf->inp,
+                              MPF_READ | CHINOL_GET_PROTO | DEFAULT_TIMEOUT,
+                              d,
+                              chino_parse_link,
+                              config_waveform_command,
+                              parse_waveform_response
+                              );
 }
-
 
 LOCAL long read_waveform(struct waveformRecord *pwf)
 {
 //  TRANSACTION *t = (TRANSACTION *) pwf->dpvt;
 //  CHINOL_LOG *d = (CHINOL_LOG *) t->device;
 
-  /*
-   * make sure that those below are cleared in the event that
-   * a multi-step transfer is terminated by an error in the
-   * middle of transacton
-   */
+    /*
+     * make sure that those below are cleared in the event that
+     * a multi-step transfer is terminated by an error in the
+     * middle of transacton
+     */
 
-  return netDevReadWriteXx((struct dbCommon *) pwf);
+    return netDevReadWriteXx((struct dbCommon *) pwf);
 }
 
-
-LOCAL long config_waveform_command(
-				   struct dbCommon *pxx,
-				   int *option,
-				   uint8_t *buf,
-				   int *len,
-				   void *device,
-				   int transaction_id
-				   )
+LOCAL long config_waveform_command(struct dbCommon *pxx,
+                                   int *option,
+                                   uint8_t *buf,
+                                   int *len,
+                                   void *device,
+                                   int transaction_id
+                                   )
 {
-  struct waveformRecord *pwaveform = (struct waveformRecord *)pxx;
-  CHINOL_LOG *d = (CHINOL_LOG *) device;
+    struct waveformRecord *pwaveform = (struct waveformRecord *)pxx;
+    CHINOL_LOG *d = (CHINOL_LOG *) device;
 
-  return chino_config_command(
-			    buf,
-			    len,
-			    pwaveform->bptr,
-			    pwaveform->ftvl,
-			    pwaveform->nelm,
-			    option,
-			    d,
-			    transaction_id
-			    );
-} 
+    return chino_config_command(buf,
+                                len,
+                                pwaveform->bptr,
+                                pwaveform->ftvl,
+                                pwaveform->nelm,
+                                option,
+                                d,
+                                transaction_id
+                                );
+}
 
-
-LOCAL long parse_waveform_response(
-				   struct dbCommon *pxx,
-				   int *option,
-				   uint8_t *buf,
-				   int *len,
-				   void *device,
-				   int transaction_id
-				   )
+LOCAL long parse_waveform_response(struct dbCommon *pxx,
+                                   int *option,
+                                   uint8_t *buf,
+                                   int *len,
+                                   void *device,
+                                   int transaction_id
+                                   )
 {
-  struct waveformRecord *pwaveform = (struct waveformRecord *)pxx;
-  CHINOL_LOG *d = (CHINOL_LOG *) device;
-  long ret;
+    struct waveformRecord *pwaveform = (struct waveformRecord *)pxx;
+    CHINOL_LOG *d = (CHINOL_LOG *) device;
 
-  ret = chino_parse_response(
-			   buf,
-			   len,
-			   pwaveform->bptr,
-			   pwaveform->ftvl,
-			   pwaveform->nelm,
-			   option,
-			   d,
-			   transaction_id
-			   );
+    long ret = chino_parse_response(buf,
+                                    len,
+                                    pwaveform->bptr,
+                                    pwaveform->ftvl,
+                                    pwaveform->nelm,
+                                    option,
+                                    d,
+                                    transaction_id
+                                    );
 
-  switch (ret)
-    {
+    switch (ret) {
     case 0:
-      pwaveform->nord = pwaveform->nelm;
+        pwaveform->nord = pwaveform->nelm;
+        break;
     default:
-      ;
+        // never reach here as chino_parse_response() always returns zero.
+        ;
     }
 
-  return ret;
+    return ret;
 }
-
-
