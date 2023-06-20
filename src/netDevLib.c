shuei/netDev

commit 0c9af841013b44f84619f093941c07118697af22

Added '&B' option for YewPLC, which supports binary-coded decimals (BCD).

src/netDevLib.c
@@ -8,7 +8,12 @@
  * in file LICENSE that is included with this distribution.
  ******************************************************************************/
 
+//
 #include <dbFldTypes.h>
+#include <alarm.h>
+#include <recGbl.h>
+
+//
 #include "drvNetMpf.h"
 #include "devNetDev.h"
 
@@ -31,6 +36,15 @@ LOCAL long toUlongVal   (uint32_t *, int, void *, int, int, int);
 LOCAL long fromUcharVal (void *, uint8_t  *, int, int, int, int);
 LOCAL long fromUshortVal(void *, uint16_t *, int, int, int, int);
 LOCAL long fromUlongVal (void *, uint32_t *, int, int, int, int);
+
+/******************************************************************************
+ *
+ ******************************************************************************/
+#define BCDMAX_BCD 39321 // 0x9999
+#define BCDMAX_INT  9999 // 0x9999
+
+#define BCDMIN_BCD     0 // 0x9999
+#define BCDMIN_INT     0 //
 
 /******************************************************************************
  *
@@ -769,6 +783,60 @@ LOCAL long fromUlongVal(void *to,
     }
 
     return OK;
+}
+
+/******************************************************************************
+ * BCD to Integer conversion
+ ******************************************************************************/
+uint32_t netDevBcd2Int(uint16_t bcd, void *precord)
+{
+    uint32_t base = 1;
+    uint32_t dec  = 0;
+
+    //if (bcd>BCDMAX_BCD) {
+    //    recGblSetSevr(precord, HIGH_ALARM, INVALID_ALARM);
+    //    return BCDMAX_INT;
+    //}
+
+    while (bcd>0) {
+        int digit = bcd & 0x000f;
+        if (digit <= 9) {
+            dec += digit * base;
+        } else {
+            // overflow
+            recGblSetSevr(precord, HIGH_ALARM, INVALID_ALARM);
+            dec += 9 * base;
+        }
+        bcd >>= 4;
+        base *= 10;
+    }
+
+    return dec;
+}
+
+/******************************************************************************
+ * Integer to BCD conversion
+ ******************************************************************************/
+uint16_t netDevInt2Bcd(int32_t dec, void *precord)
+{
+    if (dec<BCDMIN_INT) {
+        recGblSetSevr(precord, HW_LIMIT_ALARM, INVALID_ALARM);
+        return BCDMIN_BCD;
+    } else if (dec>BCDMAX_INT) {
+        recGblSetSevr(precord, HW_LIMIT_ALARM, INVALID_ALARM);
+        return BCDMAX_BCD;
+    }
+
+    uint32_t base = 0;
+    uint16_t bcd  = 0;
+
+    while (dec>0) {
+        bcd |= ((dec%10) << base);
+        dec /= 10;
+        base += 4;
+    }
+
+    return bcd;
 }
 
 /******************************************************************************

src/devLonginYewPlc.c
@@ -110,6 +110,18 @@ LOCAL long parse_longin_response(struct dbCommon *pxx,
                                       );
         plongin->val = val;
         return ret;
+    } else if (d->flag == 'B') {
+        int16_t val;
+        long ret = yew_parse_response(buf,
+                                      len,
+                                      &val,
+                                      DBF_SHORT,
+                                      1,
+                                      option,
+                                      d
+                                      );
+        plongin->val = netDevBcd2Int(val, plongin);
+        return ret;
     } else {
         int16_t val;
         long ret = yew_parse_response(buf,

src/devLongoutYewPlc.c
@@ -76,7 +76,17 @@ LOCAL long config_longout_command(struct dbCommon *pxx,
                                   option,
                                   d
                                   );
-  } else {
+    } else if (d->flag == 'B') {
+        int16_t val = netDevInt2Bcd(plongout->val, plongout);
+        return yew_config_command(buf,
+                                  len,
+                                  &val,
+                                  DBF_SHORT,
+                                  1,
+                                  option,
+                                  d
+                                  );
+    } else {
         int16_t val = plongout->val;
         return yew_config_command(buf,
                                   len,

src/devNetDev.h
@@ -67,12 +67,20 @@ long         netDevInit(void);
 long         netDevGetHostId(char *, in_addr_t *);
 uint32_t     netDevGetSelfId(void);
 long         netDevSetEvMsgLeng(struct dbCommon *, int);
+long         netDevGetIoIntInfo(int, struct dbCommon *, IOSCANPVT *);
+
+//
+uint32_t     netDevBcd2Int(uint16_t bcd, void *precord);
+uint16_t     netDevInt2Bcd(int32_t dec, void *precord);
 long         parseLinkPlcCommon(struct link *, struct sockaddr_in *,
                             char **, char **, char **, char **, char **, char **);
-long         netDevGetIoIntInfo(int, struct dbCommon *, IOSCANPVT *);
 long         fromRecordVal(void *, int, void *, int, int, int, int);
 long         toRecordVal(void *, int, int, void *, int, int, int);
 
+
+
+
+//
 TRANSACTION *netDevInitInternalIO(struct dbCommon *, struct sockaddr_in,
                                   config_command_fn_t, parse_response_fn_t, CALLBACK_fn_t, void *, int);
 

src/devYewPlc.c
@@ -248,6 +248,9 @@ LOCAL long yew_parse_link(struct link *plink,
         } else if (lopt[0] == 'U') {
             d->flag = 'U';
             LOGMSG("devYewPlc: found option to handle the data as unsigned data\n");
+        } else if (lopt[0] == 'B') {
+            d->flag = 'B';
+            LOGMSG("devYewPlc: found option to handle the data as BCD data\n");
         } else {
             errlogPrintf("devYewPlc: unsupported flag : %c\n", lopt[0]);
             return ERROR;

commit 908296ea0f326e4476c46e51bfc203ee580e76c5

Restored compatibility with EPICS base R3.14.
Compatibility with EPICS base R3.14 was broken since commit 0c9af841013b44f84619f093941c07118697af22.
We need to cast (void *) to (dbCommon *) because recGblSetServ() is macro in R3.14.

src/netDevLib.c
@@ -808,7 +808,7 @@ uint32_t netDevBcd2Int(uint16_t bcd, void *precord)
             dec += digit * base;
         } else {
             // overflow
-            recGblSetSevr(precord, HIGH_ALARM, INVALID_ALARM);
+            recGblSetSevr((dbCommon *)precord, HIGH_ALARM, INVALID_ALARM); // cast required for base R3.14
             dec += 9 * base;
         }
         bcd >>= 4;
@@ -824,10 +824,10 @@ uint32_t netDevBcd2Int(uint16_t bcd, void *precord)
 uint16_t netDevInt2Bcd(int32_t dec, void *precord)
 {
     if (dec<BCDMIN_INT) {
-        recGblSetSevr(precord, HW_LIMIT_ALARM, INVALID_ALARM);
+        recGblSetSevr((dbCommon *)precord, HW_LIMIT_ALARM, INVALID_ALARM); // cast required for base R3.14
         return BCDMIN_BCD;
     } else if (dec>BCDMAX_INT) {
-        recGblSetSevr(precord, HW_LIMIT_ALARM, INVALID_ALARM);
+        recGblSetSevr((dbCommon *)precord, HW_LIMIT_ALARM, INVALID_ALARM); // cast required for base R3.14
         return BCDMAX_BCD;
     }
 